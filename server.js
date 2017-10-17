cf8aed1e807574b3e8376e709ed38287cc264a7d
Melanie West <dhmofan@gmail.com>
2017-10-17 17:17:50 -0400

    Find headlines and links from New York Times

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const req = require("request");
-const cheer = require("cheerio");
+const request = require("request");
+const cheerio = require("cheerio");
+
+const mongojs = require("mongojs");
+
+//database configuration
+var databaseUrl = "scraper";
+var collections = ["NewYorkTimes","yCombinator"];
+
+//Hook mongojs to database
+var db = mongojs(databaseUrl,collections);
+db.on("error", function(error){
+    console.log("Database error: ",error);
+});
 
 var app = express();
 
@@ -9,6 +21,98 @@ var PORT = process.env.PORT ||3000;
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
+
+//test the home page.  No functionality yet
+app.get("/",function(req,res){
+    res.send("Hello World");
+});
+
+app.get("/all",function(req,res){
+    db.NewYorkTimes.find({},function(error,found){
+        if(error){
+            console.log(error);
+        }
+        else{
+            res.json(found);
+        }
+    });
+
+    // db.yCombinator.find({},function(error,found){
+    //     if(error){
+    //         console.log(error);
+    //     }
+    //     else{
+    //         res.json(found);
+    //     }
+    // });
+
+});
+
+
+
+
+app.get("/nyt", function (req, res) {
+    
+      request("https://www.nytimes.com", function (error, response, html) {
+    
+        var $ = cheerio.load(html);
+        var results = [];
+    
+        $("h2.story-heading").each(function (i, element) {
+          var link = $(element).find("a").attr("href");
+          var title = $(element).find("a").text();
+    
+          console.log('found an h2');
+          if (title && link) {
+            db.NewYorkTimes.insert({
+              link: link,
+              title: title
+            },
+              function (err, saved) {
+                if (err) {
+                  console.log(err);
+                }
+                else {
+                  console.log(saved);
+                }
+              });
+        };
+        });
+    
+      });
+      res.send('news scrape complete')
+      //res.json(render);
+    });
+
+
+
+
+app.get("/ycomb",function(req,res){
+    request("https://news.ycombinator.com/",function(error,response,html){
+        var $ = cheerio.load(html);
+        $(".title").each(function(i,element){
+            var title = $(this).children("a").text();
+            var link = $(this).children("a").attr("href");
+            if (title && link){
+                db.yCombinator.save({
+                    title: title,
+                    link: link
+                },
+            function(err, saved){
+                if (err){
+                    console.log(err);
+                }
+                else{
+                    console.log(saved);
+                }
+              });
+            };
+        })
+
+     });
+
+     res.send("scrape complete");
+});
 
 const exphbs = require("express-handlebars");
 app.engine("handlebars",exphbs({defaultLayout:"main"}));
@@ -18,8 +122,8 @@ app.set("view engine","handlebars");
 //which in turn will require the models
 //files
 
-require("./controllers/apiroutes");
-require("./controllers/htmlroutes");
+// require("./controllers/apiroutes");
+// require("./controllers/htmlroutes");
 
 app.listen(PORT, function(){
     console.log("App listening on PORT: "+PORT);
