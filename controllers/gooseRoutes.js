1c4c2ee815acbea641ff4c99594ba9c31b69f4d5
Melanie West <dhmofan@gmail.com>
2017-10-23 17:03:48 -0400

    Closer to working!  buttons to scrape for new articles and to display json are now working.  Remove button still only works for the first article on the list.  Now there is a note box for each article, since making the box appear was a challenge.  When I try to post a note, the article json

--- controllers/gooseRoutes.js
+++ controllers/gooseRoutes.js
@@ -51,6 +51,7 @@ module.exports = function(app){
           });
         });
         console.log("scrape complete");
+        res.redirect("/");
       });
 
 // route for displaying Articles database using handlebars
@@ -122,36 +123,11 @@ app.get("/articles", function(req, res) {
   });
   
 
-// Route for saving/updating an Article's associated Note
-// router.post("/addNote", function (req, res) {
-//   var note = {
-//       title : req.body.title,
-//       body : req.body.note
-//   }
-//   console.log(note);
-  
-//   // Create a new note and pass the req.body to the entry
-//   Note
-//     .create(note)
-//     .then(function (dbNote) {
-//       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
-//       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
-//       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
-//       return Article.findOneAndUpdate({ _id: req.body.id }, { note: dbNote._id }, { new: true });
-//     })
-//     .then(function (dbArticle) {
-//       // If we were able to successfully update an Article, send it back to the client
-//         res.json(dbArticle);
-//     })
-//     .catch(function (err) {
-//       // If an error occurred, send it to the client
-//       res.json(err);
-//     });
-// });
+
 
 app.post("/delete/:id", function (req, res) {
   // Grab every document in the Articles collection
-  Article
+  db.Article
     .remove({ _id: req.params.id })
     .then(function (dbArticle) {
       // If we were able to successfully delete Articles, redirect to saved article page
